e0820c930a37f60d9589ecb63155322991270005
Hussein <hussein.osman.@student.moringaschool.com>
2025-03-20 08:35:18 +0300

    seve charge complite

--- index.js
+++ index.js
@@ -1,48 +1,25 @@
-const cats = ["Milo", "Otis", "Garfield"];
-
-// Destructive methods
+let cats =["Milo", "Otis", "Garfield"];
 function destructivelyAppendCat(name) {
     cats.push(name);
 }
-
-function destructivelyPrependCat(name) {
+function destructivelyPrependCat(name){
     cats.unshift(name);
 }
-
-function destructivelyRemoveLastCat() {
-    cats.pop();
+function destructivelyRemoveLastCat(name){
+    cats.pop(name);
 }
-
-function destructivelyRemoveFirstCat() {
-    cats.shift();
+function destructivelyRemoveFirstCat(name){
+    cats.shift(name);
 }
-
-// Non-destructive methods
 function appendCat(name) {
-    return [...cats, name];
+    return cats.concat(name); 
 }
-
 function prependCat(name) {
-    return [name, ...cats];
+    return [name].concat(cats); 
 }
-
 function removeLastCat() {
-    return cats.slice(0, -1);
+    return cats.slice(0, cats.length - 1);
 }
-
 function removeFirstCat() {
-    return cats.slice(1);
+    return cats.slice(1); 
 }
-
-// Export functions for testing
-module.exports = {
-    cats,
-    destructivelyAppendCat,
-    destructivelyPrependCat,
-    destructivelyRemoveLastCat,
-    destructivelyRemoveFirstCat,
-    appendCat,
-    prependCat,
-    removeLastCat,
-    removeFirstCat
-};
